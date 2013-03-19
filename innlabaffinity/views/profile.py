16677dda6d19ff5450f7dcf473ccc978929d8e84
Lorenzo Setale <koalalorenzo@gmail.com>
2013-03-19 20:08:27 +0100

    Improved module to manage Profile Views

--- innlabaffinity/views/profile.py
+++ innlabaffinity/views/profile.py
@@ -1,20 +1,63 @@
-from innlabaffinity import app
+from innlabaffinity import app, db
 from innlabaffinity.decorators import *
 from innlabaffinity.objects import User, get_user
 
 from flask import render_template
+from flask import url_for
+from flask import session
+from flask import abort
+from flask import redirect
+from flask import flash
+import json
 
 @app.route("/profile/<username>")
-@cached()
 def show_profile(username):
     """Show user Profile"""
-    
-    return render_template("homepage.html")
-    
+    profile = User()
+    profile.database = db
+    profile.username = username
+    if not profile.already_exist():
+        flash("Username not exists")
+        redirect(url_for("homepage"))
+
+    profile.load(username)
+    return render_template("profile.html", profile=user)
+
 @app.route("/profile/<username>/edit")
-@cached()
 def edit_profile(username):
     """Edit Profile"""
+    profile = get_user(session['user'], db)
+    return render_template("edit_profile.html", profile=profile)
+
+
+# API
+
+@app.route("/api/profile/<username>")
+def api_get_profile(username):
+    """return Profile in json format"""
+    return ""
     
+@app.route("/api/profile/<username>/vote/new")
+def api_vote_profile(username):
+    """Vote Profile"""
+    return ""
     
-    return render_template("homepage.html")
+@app.route("/api/profile/<username>/vote")
+def api_list_vote_profile(username):
+    """Get votes Profile"""
+    return ""
+
+@app.route("/api/profile/<username>/skills/set")
+def api_set_skills_profile(username):
+    """Set Skills Profile"""
+    return ""
+    
+@app.route("/api/profile/<username>/interests/set")
+def api_set_interests_profile(username):
+    """Set interests Profile"""
+    return ""
+    
+@app.route("/api/profile/<username>/description/set")
+def api_set_description_profile(username):
+    """Set Description Profile"""
+    return ""
