52ae52e12dd536da7c85b0dccc6b68b9e99937b2
erlangb <erlangb@yahoo.com>
2013-03-24 19:08:04 +0100

    firstCommit

--- configuration.py
+++ configuration.py
@@ -4,8 +4,8 @@
 
 MONGO_HOST = "127.0.0.1"
 MONGO_PORT = 27017
-MONGO_USERNAME = "innlabaffinity"
-MONGO_PASSWORD = "1234567890"
+MONGO_USERNAME = ""
+MONGO_PASSWORD = ""
 MONGO_DB = "innlabaffinity"
 
 FLASK_SECRET_KEY = "dr.kaiy °ç^^ìòopshtl .ouoòs3£jfs"
